134a3e3651ff837ded652b9856961707822f7e78
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-06-02 16:43:24 +0300

    Add page content

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -8,57 +8,324 @@ export default function Home(props: AppProps) {
 
   return (
     <div className="container">
-      <img src="/assets/safesphere-logo.svg" />
-      <Navigation />
+      <div className={styles.pageHeader}>
+        <a href="./">
+          <img src="/assets/safesphere-logo.svg" alt="SafeSphere logo" />
+        </a>
+        <Navigation />
+      </div>
 
       <main>
-        <section className={styles.section}>
-          <h2 className="subheading">
-            Bringing the world closer but not too close.
-          </h2>
+        {/**
+         * Intro section
+         */}
+        <section className={`${styles.section} ${styles.columns}`}>
+          <div>
+            <h2 className="subheading">
+              Bringing the world closer
+              <br />
+              <span className="accent">but not too close.</span>
+            </h2>
+            <br />
+
+            <p>
+              SafeSphere's mission is to help institutions and businesses create
+              a safe environment for employees and their customers. Our
+              proprietary technology helps companies protect and empower people
+              in their workplace, by identifying proximity risks with zero data
+              stored.
+            </p>
+
+            <p>
+              <strong>
+                It's time to give people back their safety and their lives. We
+                help you create a SafeSphere.
+              </strong>
+            </p>
+            <br />
+            <a href="#about" className="button">
+              Learn More
+            </a>
+          </div>
+          <div className={`${styles.firstColumn}`}>Video</div>
         </section>
 
-        <section id="about" className={styles.section}>
-          <header>
-            <h2 className="h2">About</h2>
-            <p className="subheading">What is SafeSphere</p>
-          </header>
+        {/**
+         * About section
+         */}
+        <section id="about" className={`${styles.section} ${styles.columns}`}>
+          <div>
+            <header>
+              <h2 className="h2">About</h2>
+              <br />
+              <p className="subheading">What is SafeSphere</p>
+            </header>
+            <br />
+            <p>
+              SafeSphere is a revolutionary technology that accurately
+              determines the distance between two or more smartphone users over
+              time when integrated with your mobile app.
+            </p>
+            <p>
+              <strong>
+                The state-of-the-art technology constantly analyzes proximity
+                data between smartphones.
+              </strong>
+            </p>
+          </div>
+          <div>illustration</div>
         </section>
 
-        <section id="how-it-works" className={styles.section}>
-          <header>
-            <h2 className="h2">How it works</h2>
-            <p className="subheading">A sound solution</p>
-          </header>
+        {/**
+         * How it works section
+         */}
+        <section
+          id="how-it-works"
+          className={`${styles.section} ${styles.columns}`}
+        >
+          <div>illustration</div>
+          <div>
+            <header>
+              <h2 className="h2">How it works</h2>
+              <br />
+              <p className="subheading">A sound solution</p>
+            </header>
+            <br />
+            <ul>
+              <li>
+                Using precise acoustic signals, SafeSphere’s patent-pending
+                proprietary algorithms determine how close users are to each
+                other, alerting them to keep a safe distance.
+              </li>
+              <li>
+                The indicator is highly-customizable and works with any mobile
+                app technology to help provide a safe environment. (where we
+                congregate, live, work, shop, travel, play, learn, and worship){" "}
+              </li>
+              <li>
+                Thanks to its unique algorithm, SafeSphere minimizes exposure in
+                unsafe environments by monitoring short distances between people
+                over time.
+              </li>
+              <li>
+                No other technology can deliver proximity with accuracy for
+                short distances like SafeSphere on mobile devices.
+              </li>
+            </ul>
+          </div>
         </section>
 
-        <section id="use-cases" className={styles.section}>
-          <header>
-            <h2 className="h2">Use cases</h2>
-            <p className="subheading">Who would benefit?</p>
-          </header>
+        {/**
+         * Use cases section
+         */}
+        <section
+          id="use-cases"
+          className={`${styles.section} ${styles.columns}`}
+        >
+          <div>
+            <header>
+              <h2 className="h2">Use cases</h2>
+              <br />
+              <p className="subheading">Who would benefit?</p>
+            </header>
+            <br />
+            <p>
+              <strong>
+                SafeSphere was designed from the ground up to help keep everyone
+                safe: employees, customers, students, teachers, senior citizens,
+                families, and friends.
+              </strong>
+            </p>
+            <p>
+              By integrating SafeSphere’s technology, your mobile app will
+              notify you when crowded areas become unsafe. With minimal costs
+              and physical changes to your facilities, users can create safer
+              movement flows during daily activities.
+            </p>
+          </div>
+          <div>
+            <ul>
+              <li>Supermarkets and retail spaces</li>
+              <li>Sports arenas and Gyms</li>
+              <li>Universities</li>
+              <li>Restaurants</li>
+              <li>Office buildings and factories</li>
+              <li>Healthcare institutions</li>
+              <li>Airports and train stations</li>
+              <li>Public parks</li>
+            </ul>
+          </div>
         </section>
 
-        <section id="user-privacy" className={styles.section}>
-          <header>
-            <h2 className="h2">User Privacy</h2>
-            <p className="subheading">No quid pro quo</p>
-          </header>
+        {/**
+         * User privacy section
+         */}
+        <section
+          id="user-privacy"
+          className={`${styles.section} ${styles.columns}`}
+        >
+          <div>illustration</div>
+          <div>
+            <header>
+              <h2 className="h2">User Privacy</h2>
+              <br />
+              <p className="subheading">No quid pro quo</p>
+            </header>
+            <br />
+            <p>
+              SafeSphere is here to help you create a secure environment in a
+              transparent, easy-to-implement, and scalable way.
+            </p>
+            <p>
+              <strong>
+                We tackle safety with a 360-approach: No data stored and
+                complete user privacy. Our priority is employee and customer
+                well-being all around.
+              </strong>
+            </p>
+          </div>
         </section>
 
+        {/**
+         * Founding team section
+         */}
         <section id="founding-team" className={styles.section}>
-          <header>
-            <h2 className="h2">SafeSphere’s Founding Team</h2>
-            <p className="subheading">Where STEM Solves Everyday Challenges </p>
-          </header>
+          <div className={styles.columns}>
+            <div>
+              <header>
+                <h2 className="h2">SafeSphere’s Founding Team</h2>
+                <br />
+                <p className="subheading">
+                  Where STEM Solves Everyday Challenges{" "}
+                </p>
+              </header>
+            </div>
+            <div>
+              <p>
+                <strong>
+                  SafeSphere is the result of a major collaborative effort
+                  between business leaders, medical professionals, engineers,
+                  and scientists.
+                </strong>
+              </p>
+              <p>
+                We gathered some real brainpower to create a new, accurate, and
+                inexpensive way to create a safe space for our sphere of
+                interest, on your mobile device. We are a joint venture between
+                Espresoh Tech, a US-based product engineering company, and
+                Alpine Consolidated, investing in first-to-market businesses for
+                more than 25 years.
+              </p>
+            </div>
+          </div>
+
+          <section>
+            <h3 className="h2">Management</h3>
+            <ul className={styles.team}>
+              <li>
+                <strong className={styles.teamName}>Elan Blutinger</strong>
+                <span className={styles.teamRole}>Chief Executive Officer</span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>Marcel Barbulescu</strong>
+                <span className={styles.teamRole}>
+                  President and Chief Financial Officer
+                </span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>Cristian Bursasiu</strong>
+                <span className={styles.teamRole}>Chief Product Officer</span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>Daniel Markovits</strong>
+                <span className={styles.teamRole}>
+                  Chief Technology Officer
+                </span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>Raluca Popa</strong>
+                <span className={styles.teamRole}>Chief Marketing Officer</span>
+              </li>
+            </ul>
+          </section>
+
+          <section>
+            <h3 className="h2">Advisors</h3>
+            <ul className={styles.team}>
+              <li>
+                <strong className={styles.teamName}>Erik J Blutinger</strong>
+                <span className={styles.teamRole}>MD</span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>Omar Maniya</strong>
+                <span className={styles.teamRole}>MD</span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>Jonathan Moreno</strong>
+                <span className={styles.teamRole}>Pd. H</span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>Alexander Sackheim</strong>
+                <span className={styles.teamRole}>MD</span>
+              </li>
+              <li>
+                <strong className={styles.teamName}>TBD</strong>
+                <span className={styles.teamRole}>
+                  Ph.D. Acoustical Engineer
+                </span>
+              </li>
+            </ul>
+          </section>
         </section>
 
-        <section id="contact-us" className={styles.section}>
-          <header>
-            <h2 className="subheading">SafeSphere your business</h2>
-          </header>
+        {/**
+         * Contact us section
+         */}
+        <section
+          id="contact-us"
+          className={`${styles.section} ${styles.columns}`}
+        >
+          <div>
+            <header>
+              <h2 className="subheading">SafeSphere your business</h2>
+            </header>
+            <br />
+            <p>
+              <strong>
+                It’s time to rethink how the world works and get back to
+                business.
+              </strong>
+            </p>
+            <p>
+              SafeSphere will shortly be available to businesses and
+              institutions – sign up now for important updates.
+            </p>
+            <form action="/">
+              <input type="email" placeholder="Enter your email" />
+              <button type="submit" className="button">
+                Subscribe
+              </button>
+            </form>
+          </div>
+
+          <form action="/">
+            <input type="text" placeholder="name" />
+            <input type="email" placeholder="email" />
+            <input type="tel" placeholder="phone" />
+            <input type="text" placeholder="How can we help?" />
+            <button type="submit" className="button">
+              Contact Us
+            </button>
+          </form>
         </section>
       </main>
+
+      <footer className={styles.pageFooter}>
+        <span>
+          Designed and developed by <strong>[e-spres-oh]</strong>
+        </span>
+        <span>&copy;Safe Sphere, 2020</span>
+      </footer>
     </div>
   );
 }
