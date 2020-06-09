4c53a3997e60d639f30c3c8978524f7141620544
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-06-09 13:22:07 +0300

    Shrink header when scrolled

--- pages/index/components/header.tsx
+++ pages/index/components/header.tsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "./navigation";
 import Hamburger from "./hamburger";
 import styles from "./header.module.scss";
 
 function Header() {
   const [showMenu, toggleMenu] = useState(false);
+  const [scrolled, toggleScrolled] = useState(false);
+
+  function toggleScrolledMenu() {
+    const scrollY = window && window.scrollY;
+    toggleScrolled(scrollY > 70);
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", toggleScrolledMenu);
+    return () => document.removeEventListener("scroll", toggleScrolledMenu);
+  });
 
   return (
-    <nav role="navigation" className={styles.header} data-menu-show={showMenu}>
+    <nav
+      role="navigation"
+      className={styles.header}
+      data-menu-show={showMenu}
+      data-menu-scrolled={scrolled}
+    >
       <a href="./" className={styles.logo}>
         <img src="/assets/safesphere-logo.svg" alt="SafeSphere logo" />
       </a>
--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { AppProps } from "next/app";
 
 import Header from "./components/header";
