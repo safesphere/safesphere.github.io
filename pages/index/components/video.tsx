b70ab3751ff9824cbd11ab5237bc3f566d0eaa08
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-06-17 13:46:52 +0300

    Update video.tsx

--- pages/index/components/video.tsx
+++ pages/index/components/video.tsx
@@ -40,7 +40,7 @@ function Video(props: Props) {
         <div className={styles.videoPlayer}>
           <iframe
             className={styles.embededVideo}
-            src={`https://www.youtube-nocookie.com/embed/${youtubeId}?&autoplay=1`}
+            src={`https://www.youtube-nocookie.com/embed/${youtubeId}?autoplay=1`}
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
